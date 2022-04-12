b7d10781d4cf8df7a98c984585ffdfacbe7f3f19
lvREES <lukasz1234@onet.eu>
2022-04-12 15:02:00 +0200

    Komentarz

---
+++ nww.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+using namespace std;
+
+int NWD(int a, int b)
+{
+	int pom;
+
+	while (b != 0)
+	{
+		pom = b;
+		b = a % b;
+		a = pom;
+	}
+	return a;
+}
+
+int main()
+{
+	unsigned int a, b;
+
+	cout << "Podaj pierwsza liczbe: "; cin >> a;
+	cout << "Podaj druga liczbe: "; cin >> b;
+
+	//wyznaczenie NWW
+	cout << "Najmniejsza wspolna wielokrotnosc:(" << a << ", " << b << ") = " << a / NWD(a, b) * b << endl;
+
+	cin.ignore();
+	cin.get();
+
+	return 0;
+}
