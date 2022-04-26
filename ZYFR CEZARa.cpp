1ab361dd88faea0b21441618b9bebaf9424f1f79
lvREES <lukasz1234@onet.eu>
2022-04-26 14:37:07 +0200

    Komentarz

--- ZYFR CEZARa.cpp
+++ ZYFR CEZARa.cpp
@@ -4,30 +4,70 @@ using namespace std;
 
 chrono::duration<double> elapsed_seconds;
 
+void szyfrowanie(int klucz, char tab[]) {
+
+	int dl = strlen(tab);
+
+	if (!(klucz >= -26 and klucz <= 26)) {
+		return;
+	}
+
+	if (klucz >= 0) {
+		for (int i = 0; i < dl; i++) {
+			if (tab[i] + klucz <= 'z') {
+				tab[i] += klucz;
+			}
+			else
+			{
+				tab[i] += (klucz - 26);
+			}
+		}
+	}
+	else
+	{
+		for (int i = 0; i < dl; i++) {
+			if (tab[i] + klucz >= 'a') {
+				tab[i] += klucz;
+			}
+			else
+			{
+				tab[i] += (klucz + 26);
+			}
+		}
+	}
+}
+
+
 int main()
 {
-	int a, f, f0 = 0, f1 = 1;
-
 	chrono::system_clock::time_point start = chrono::system_clock::now(); // czas start
 
-	cout << "Podaj ile liczb ma wyswietlic: ";
-	cin >> a;
 
-	for (int i = 0; i < a; i++)
-	{
-		if (i > 1) {
-			f = f0 + f1;
-			f0 = f1;
-			f1 = f;
-		}
-		else
-		{
-			f = i;
-		}
-		cout << f << endl;
-	}
+	int dl, klucz;
+	char tab[200];
+
+	cout << "Podaj klucz: ";
+	cin >> klucz;
+	cout << "Podaj wyraz: ";
+	cin >> tab;
+	cout << endl;
+
+
+
+	szyfrowanie(klucz, tab);
+	cout << "Zaszyfrowane: " << tab;
+
+	cout << endl;
+
+	szyfrowanie(-klucz, tab);
+	cout << "Odszyfrowane: " << tab << endl;
+
 	chrono::system_clock::time_point end = chrono::system_clock::now(); // czas stop
 	elapsed_seconds += end - start; // obliczenie czasu
 
-	cout << std::chrono::duration_cast<std::chrono::milliseconds>(elapsed_seconds).count() << "\t milliseconds\n";
+	cout << std::chrono::duration_cast<std::chrono::seconds>(elapsed_seconds).count() << "\t seconds\n";
+
+
+
+
 }
